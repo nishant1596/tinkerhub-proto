6e63f081d24eaf8ce5d0f2e2489f9b01fac9c701
nishant <happysoni777@gmail.com>
2019-04-20 16:23:25 +0530

    integrate with vue

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
+$(function(){
+var sheetUrl = 'https://spreadsheets.google.com/feeds/cells/1M-444B44qT78GDJH7MiGJ363FjVxA06-MmZVBqJ3IkQ/1/public/full?alt=json';
+$.getJSON(sheetUrl, function(data){
+  var entry = data.feed.entry;
+  var name = []; // the leftmost column of the Google Sheets
+  var college = []; // second column
+  var phone_no = []; // third column
+  var technology=[]; //fourth column
+  var level=[];      //fifth column
+  var photo=[]; //sixth column
+
+for (var i = 0; i < entry.length; i += 6){
+    // entry[i].content.$t retrieves the content of each cell
+    name.push(entry[i].content.$t);
+    college.push(entry[i+1].content.$t);
+    phone_no.push(entry[i+2].content.$t);
+    technology.push(entry[i+3].content.$t);
+    level.push(entry[i+4].content.$t);
+    photo.push(entry[i+5].content.$t);
+  }
+  console.log(name);
+  console.log(college);
+  console.log(phone_no);
+  console.log(technology);
+  console.log(level);
+  console.log(photo);
+
+})
+});
+
+// for (var i = 0; i < entry.length; i++) {
+//
+// }
+
+
+
 var $message = 'Learn to Teach \n Teach to Learn'.split('').reverse();
 
 // Set the frequency of your 'pops'
@@ -16,10 +52,5 @@ var outputSlowly = setInterval(function() {
 }, $timeout);
 
 
-function myMap() {
-var mapProp= {
-  center:new google.maps.LatLng(51.508742,-0.120850),
-  zoom:5,
-};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
+
+// Getting data as Json from spreadsheet
